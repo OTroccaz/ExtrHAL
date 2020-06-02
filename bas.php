1ed23a239300bccc2867e940edab77d9fb7b204c
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2020-06-02 13:15:28 +0200

    Update bas.php

--- bas.php
+++ bas.php
@@ -1,2 +1,2 @@
 <hr style="color: #467666; height: 1px; border-width: 1px; border-top-color: #467666; border-style: inset;">
-<a target='_blank' href='https://halur1.univ-rennes1.fr/credits-et-mentions-legales.php'>Crédits et mentions légales</a> - © 2020 - Université de Rennes 1 / CNRS - Tous droits réservés
+<a target='_blank' href='https://halur1.univ-rennes1.fr/credits-et-mentions-legales.php'>Crédits et mentions légales</a> - © <?php echo(date('Y', time()));?> - Université de Rennes 1 / CNRS - Tous droits réservés
